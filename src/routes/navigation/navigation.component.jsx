488acebb4aa0bf60e64a7be050cbaa09e446c867
Nitesh-6 <sainitesh6@gmail.com>
2023-08-02 17:10:41 +0530

    created and added Routes

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,24 @@
+import { Outlet, Link } from "react-router-dom";
+import { Fragment } from "react";
+import "./navigation.styles.scss"
+import crownLogo from "../../assets/crown.svg";
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <img src={crownLogo} alt="Crown Logo" className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            Shop
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
